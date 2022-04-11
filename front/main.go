f2d6e28147fac2ad4a5f28746d1abb84de59cdc5
tokizuoh <tokizo819@gmail.com>
2022-04-12 03:56:50 +0900

    MultipartReader()でmultipart/form-dataの値を受け取る処理を追加

--- front/main.go
+++ front/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		name := r.FormValue("name")
-		color := r.FormValue("color")
-		log.Println(name, color)
+		mr, _ := r.MultipartReader()
+		for {
+			p, err := mr.NextPart()
+			if err == io.EOF {
+				break
+			}
+
+			// input type が "text" の場合は FileName() が空になる
+			if len(p.FileName()) == 0 {
+				d := make([]byte, 1024)
+				_, _ = p.Read(d)
+				fmt.Println(p.FormName(), ":", string(d), p.Header.Get("Content-Type"))
+			} else {
+				fmt.Println(p.FormName(), ":", p.FileName(), p.Header.Get("Content-Type"))
+			}
+		}
+
+		// POST したときに localhost:8080/users にとどまってしまうのでルートにリダイレクト
 		http.Redirect(w, r, "/", 301)
 	})
 	http.ListenAndServe(":8080", nil)
